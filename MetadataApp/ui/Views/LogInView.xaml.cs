de0d9d6fb7900e8b7d19c20dbcb78dea60dc1c2f
avnva <voynova.03@inbox.ru>
2023-03-28 15:59:12 +0700

    Change LogIn Window design

--- MetadataApp/ui/Views/LogInView.xaml.cs
+++ MetadataApp/ui/Views/LogInView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.Windows;
+using System.Windows.Input;
 using MetadataApp.ui.ViewModels;
 
 namespace MetadataApp;
@@ -18,11 +19,37 @@ public partial class LogInView : Window
         (DataContext as ViewModelBase).Close += () => { Close(); };
         (DataContext as ViewModelBase).MessageBoxRequest +=
             ViewMessageBoxRequest;
+
+        EventManager.RegisterClassHandler(typeof(Window), KeyDownEvent, new KeyEventHandler(MainKeyDown));
+        languageText.Text = "Язык раскладки: " + Language.GetEquivalentCulture().DisplayName;
+        capsLockPressed.Text = "Клавиша Caps Lock нажата";
+        bool isCapsLockToggled = Keyboard.IsKeyToggled(Key.CapsLock);
+        if (isCapsLockToggled)
+            capsLockPressed.Text = "Клавиша Caps Lock нажата";
+        else
+            capsLockPressed.Text = "";
+        InputLanguageManager.Current.InputLanguageChanged +=
+               new InputLanguageEventHandler((sender, e) =>
+               {
+                   languageText.Text = "Язык раскладки: " + e.NewLanguage.DisplayName;
+               });
     }
 
     private void ViewMessageBoxRequest(object sender, MessageBoxEventArgs e)
     {
         e.Show();
+    }
+
+    private void MainKeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.CapsLock)
+        {
+            bool isCapsLockToggled = Keyboard.IsKeyToggled(Key.CapsLock);
+            if (isCapsLockToggled)
+                capsLockPressed.Text = "Клавиша Caps Lock нажата";
+            else
+                capsLockPressed.Text = "";
+        }
     }
 
     private void OnClose(object sender, CancelEventArgs e)
